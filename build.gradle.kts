d7a11616eaae0e08c354349677b1b46a822a07e0
Nullicorn <bennullicorn@gmail.com>
2022-02-13 20:18:54 -0500

    Move publishing tasks to separate gradle script
    Signed-off-by: Nullicorn <bennullicorn@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.dokka.gradle.DokkaTask
-
 plugins {
     // Kotlin language & docs
     kotlin("multiplatform") version "1.6.10"
@@ -10,6 +8,8 @@ plugins {
     id("signing")
     id("maven-publish")
 }
+
+apply(from = "gradle/publish.gradle.kts")
 
 group = "me.nullicorn"
 version = "0.0.1-SNAPSHOT"
@@ -85,69 +85,4 @@ kotlin {
         nodejs()
         browser()
     }
-}
-
-// KDoc HTML generation; required for Maven Central.
-val dokkaHtml by tasks.getting(DokkaTask::class)
-val javadocJar by tasks.registering(Jar::class) {
-    dependsOn(dokkaHtml)
-    archiveClassifier.set("javadoc")
-    from(dokkaHtml.outputDirectory)
-}
-
-publishing {
-    // Add extra metadata for the JVM jar's pom.xml.
-    publications.withType<MavenPublication> {
-        val authorUrl = project.extra["author.url"] as String
-        val projectName = project.extra["name"] as String
-        val projectUrl = "$authorUrl/$projectName"
-
-        artifact(tasks["javadocJar"])
-
-        pom {
-            url.set("https://$projectUrl")
-
-            developers {
-                developer {
-                    name.set("TheNullicorn")
-                    email.set("bennullicorn@gmail.com")
-                }
-            }
-
-            licenses {
-                license {
-                    name.set("MIT License")
-                    url.set("https://opensource.org/licenses/mit-license.php")
-                }
-            }
-
-            scm {
-                url.set("https://$projectUrl/tree/main")
-                connection.set("scm:git:git://$projectUrl.git")
-                developerConnection.set("scm:git:ssh://$projectUrl.git")
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            name = "ossrh"
-
-            val repoId =
-                if (version.toString().endsWith("SNAPSHOT")) "snapshot"
-                else "release"
-            url = uri(project.extra["repo.$repoId.url"] as String)
-
-            credentials {
-                // Both values should be set in "~/.gradle/gradle.properties".
-                username = (project.extra["ossrhUsername"] as String)
-                password = (project.extra["ossrhPassword"] as String)
-            }
-        }
-    }
-}
-
-// Sign all of our artifacts for Nexus.
-signing {
-    sign(publishing.publications)
 }
